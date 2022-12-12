94c2164d2a35b5ff980fca6638087242042f367a
Eddie Jaoude <eddie@jaoudestudios.com>
2022-12-12 13:41:16 +0000

    pwa wip

--- next.config.js
+++ next.config.js
@@ -16,3 +16,13 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+const withPWA = require("next-pwa");
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    disable: process.env.NODE_ENV === "development",
+    skipWaiting: true,
+  },
+});
